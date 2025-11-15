3db964b08122d229ce23b981cb7e1e5d431ceaea
Alexandre da Silva <lexrupy@gmail.com>
2025-11-14 23:50:38 -0300

    local

--- bin/multimonitori_bk.sh
+++ bin/multimonitori_bk.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/bash
-#xrandr --newmode 1920x1080_60  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
-#xrandr --addmode HDMI1 1920x1080_60
+
 QTD_MONITORS=$(xrandr | grep -sw connected | wc -l)
-if [ $QTD_MONITORS -gt 1 ]; then
+if [ $QTD_MONITORS -gt 2 ]; then
+	FIRST=$(xrandr | grep -sw connected | awk '{print $1}' | sed -n "1p")
+	SECOND=$(xrandr | grep -sw connected | awk '{print $1}' | sed -n "2p")
+	THIRD=$(xrandr | grep -sw connected | awk '{print $1}' | sed -n "3p")
+  xrandr \
+  	--output $FIRST --primary --mode 1366x768 --pos 2474x0 --rotate normal \
+  	--output $SECOND --mode 1920x1080 --pos 0x768 --rotate normal \
+  	--output $THIRD --mode 1920x1080 --pos 1920x768 --rotate normal
+elif [ $QTD_MONITORS -gt 1 ]; then
 	FIRST=$(xrandr | grep -sw connected | awk '{print $1}' | sed -n "1p")
 	SECOND=$(xrandr | grep -sw connected | awk '{print $1}' | sed -n "2p")
 	#
@@ -12,3 +19,5 @@ if [ $QTD_MONITORS -gt 1 ]; then
 	# Tela notebook na esquerda
 	xrandr --output $FIRST --primary --mode 1366x768 --pos 0x0 --rotate normal --output $SECOND --mode 1920x1080 --pos 1366x0 --rotate normal
 fi
+
+
